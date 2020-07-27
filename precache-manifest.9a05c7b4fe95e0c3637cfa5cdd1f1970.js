8d9ec82263e5aef0b549e0e6de86574f46a559a7
Sergejs Basangovs <sergejs.basangovs@ingresso.co.uk>
2020-07-27 20:52:52 +0300

    Updates

---
+++ precache-manifest.9a05c7b4fe95e0c3637cfa5cdd1f1970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ceef5c0609a2a27a787eb853016e7c",
+    "url": "/xkcd-comix-app/index.html"
+  },
+  {
+    "revision": "80c64624dda09541c140",
+    "url": "/xkcd-comix-app/static/css/main.b6d47e71.chunk.css"
+  },
+  {
+    "revision": "b02bf80b4166005d6f98",
+    "url": "/xkcd-comix-app/static/js/2.d561514e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/xkcd-comix-app/static/js/2.d561514e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c64624dda09541c140",
+    "url": "/xkcd-comix-app/static/js/main.852bc00d.chunk.js"
+  },
+  {
+    "revision": "195a8b7b11a6bbd54614",
+    "url": "/xkcd-comix-app/static/js/runtime-main.70ec30f9.js"
+  }
+]);
